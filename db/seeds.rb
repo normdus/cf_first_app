b147ad11174d42c1f2f4dea2754559322f9c77cf
Norman Duchene <norman.duchene@comcast.net>
2016-02-03 19:54:04 +0000

    4.2 DB Migration Done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-# Examples:
+# How to: http://www.xyzpub.com/en/ruby-on-rails/3.2/seed_rb.html
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#  Product.create({"name"=>"", "description"=>"desc", "image_url"=>"", "color"=>"color", "price"=>12000})
+# $ rake db:seed
+#
+Product.create({"name"=>"Forty Eight", "description"=>"desc", "image_url"=>"forty-eight.jpg", "color"=>"color", "price"=>12000})
+Product.create({"name"=>"Street Bob", "description"=>"desc", "image_url"=>"street-bob.jpg", "color"=>"color", "price"=>12000})
+Product.create({"name"=>"1200 Custom", "description"=>"desc", "image_url"=>"1200-custom.jpg", "color"=>"color", "price"=>12000})
+Product.create({"name"=>"Low Rider", "description"=>"desc", "image_url"=>"low-rider.jpg", "color"=>"color", "price"=>12000})
+Product.create({"name"=>"Fat Boy", "description"=>"desc", "image_url"=>"fat-boy.jpg", "color"=>"color", "price"=>12000})
+Product.create({"name"=>"Road King", "description"=>"desc", "image_url"=>"road-king.jpg", "color"=>"color", "price"=>12000})
+
+
+
+
