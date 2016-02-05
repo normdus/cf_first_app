c47cb6886514606455e68b119ea9a8089887d672
Norman Duchene <norman.duchene@comcast.net>
2016-02-05 18:55:58 +0000

    5.1 Routes Done

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,4 +3,9 @@ class StaticPagesController < ApplicationController
   def landing_page
     @products = Product.limit(3)  # this instance variable is passed to landing_page
   end
+
+# 5.1 Routes redirect...
+  def about
+#    redirect_to "/static_pages/contact"
+  end
 end
