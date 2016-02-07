cfa9b10d0de7f1d818196b8a6ff7f1d6e8d6aea4
Norman Duchene <norman.duchene@comcast.net>
2016-02-07 18:55:46 +0000

    5.3 Email 2nd

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+  # Sendgrid Plugin for email setup ....
+  ActionMailer::Base.smtp_settings = {
+    :address => 'smtp.sendgrid.net',
+    :port => '587',
+    :authentication => :plain,
+    :user_name => ENV['rails@careerfoundry.nduchene.info'],
+    :password => ENV['norm3488'],
+    :domain => 'heroku.com',
+    :enable_starttls_auto => true
+  }
